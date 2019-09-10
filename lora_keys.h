853b04d44e012ea9666072e5b210f3a4af934a50
M52401 <Radu.Toma@microchip.com>
2019-09-10 11:23:37 +0200

    Update Lora keys for desk device
    There were some issues logging into the LoRa network. The device was not showing up in TTN. Changing the  device keys seemed to have fixed it.

--- lora_keys.h
+++ lora_keys.h
@@ -20,9 +20,9 @@ const char my_appkey[] = "EE9B2EC14296FD4FE856FDF6A4C9898C";
 
 #elif ABP
 
-const char my_devaddr[] = "26051AB0";
-const char my_nwkskey[] = "7E22D4C90358DC4C2B3C92E27C37A06F";
-const char my_appskey[] = "F0982277D42CA9CC12DEEBE8ACF826B2";
+const char my_devaddr[] = "26011DFF";
+const char my_nwkskey[] = "9CA51BE2BBC59ECFE12D94277ED24F24";
+const char my_appskey[] = "82B3BF83DFCBE7F8A5E5639CF62AB40B";
 
 #endif
 
